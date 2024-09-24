71989b8b2b4fbd0e78aa32ad617f010a60c87eb2
WassimGaha <wassim.gaha@univ-tlse3.fr>
2024-09-24 18:06:49 +0200

    TP1 fini

--- src/histoire/Scenario.java
+++ src/histoire/Scenario.java
@@ -6,14 +6,16 @@ public class Scenario {
 
 	public static void main(String[] args) {
 		Gaulois asterix = new Gaulois("Astérix", 8);
+		Gaulois obelix = new Gaulois("Obélix", 25);
 		Romain minus = new Romain("Minus", 6);
 		Druide panoramix = new Druide("Panoramix", 5, 10);
 		panoramix.parler("Je vais aller préparer une petite potion...");
 		panoramix.preparerPotion();
 		panoramix.booster(obelix);
-		asterix.parler("Bonjour à tous");
+		obelix.parler("Par Bélénos, ce n'est pas juste!");
+		panoramix.booster(asterix);
+		asterix.parler("Bonjour");
 		minus.parler("UN GAU... UN GAUGAU...");
-		asterix.frapper(minus);
 		asterix.frapper(minus);
 	}
 
